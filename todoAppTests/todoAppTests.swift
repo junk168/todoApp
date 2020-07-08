e6b4c0a628ecdc8fbcd8e884179adac0edaa7250
junk <kagerow168@gmail.com>
2020-07-08 10:50:04 +0900

    Initial Commit

---
+++ todoAppTests/todoAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  todoAppTests.swift
+//  todoAppTests
+//
+//  Created by junk on 2020/07/08.
+//  Copyright © 2020 junk. All rights reserved.
+//
+
+import XCTest
+@testable import todoApp
+
+class todoAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
